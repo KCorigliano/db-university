93ac3f69ae5ee68f855fb60e64ada1662ab08b92
KCorigliano <corigliano98@hotmail.com>
2022-02-23 15:50:03 +0100

    2 query group

--- queryGroupBy.sql
+++ queryGroupBy.sql
@@ -4,3 +4,10 @@ SELECT COUNT(`id`), YEAR(`enrolment_date`)
 FROM `students`
 GROUP BY YEAR(`enrolment_date`)
 ORDER BY COUNT(`id`) DESC;
+
+-- 2 query
+
+SELECT COUNT(`id`), `office_address`
+FROM `teachers`
+GROUP BY `office_address`
+ORDER BY COUNT(`id`)
