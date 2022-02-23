d5bbb4f1f3682ced84a82e747597e892cb43b6de
KCorigliano <corigliano98@hotmail.com>
2022-02-23 16:23:56 +0100

    2 query

--- queryJoin.sql
+++ queryJoin.sql
@@ -4,11 +4,15 @@ SELECT `students`.`name`, `students`.`surname`, `degrees`.`name`
 FROM `degrees`
 JOIN `students`
 ON `degrees`.`id`=`students`.`degree_id`
-WHERE `degrees`.`name` LIKE "corso di laurea in economia"
+WHERE `degrees`.`name` LIKE "corso di laurea in economia";
 
 -- 2 query
 
-
+SELECT `departments`.`name`, `degrees`.`name`, `degrees`.`level`
+FROM `departments`
+JOIN `degrees`
+ON `departments`.`id`=`degrees`.`department_id`
+WHERE `departments`.`name` LIKE "dipartimento di neuroscienze";
 
 -- 3 query
 
